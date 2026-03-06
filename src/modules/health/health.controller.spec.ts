652d04b79e469e0085a2c3d733a52f7797b87f09
catcode <catcode0101@gmail.com>
2026-03-06 21:19:34 +0700

    test: add unit testing on health module

--- src/modules/health/health.controller.spec.ts
+++ src/modules/health/health.controller.spec.ts
@@ -1,18 +1,63 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HealthController } from './health.controller';
+import { HealthService } from './health.service';
+
+import type { HealthResponse } from './interfaces/response.interface';
 
 describe('HealthController', () => {
-  let controller: HealthController;
+	let controller: HealthController;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [HealthController],
-    }).compile();
+	const mockHealthService = {
+		apiCheck: jest.fn(),
+		dbCheck: jest.fn(),
+	};
 
-    controller = module.get<HealthController>(HealthController);
-  });
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			controllers: [HealthController],
+			providers: [
+				{
+					provide: HealthService,
+					useValue: mockHealthService,
+				},
+			],
+		}).compile();
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+		controller = module.get<HealthController>(HealthController);
+		jest.clearAllMocks();
+	});
+
+	it('should be defined', () => {
+		expect(controller).toBeDefined();
+	});
+
+	const mockResponse: HealthResponse = {
+		status: 'ok',
+		uptime: 10,
+		timestamp: 'sat06032026',
+	};
+
+	// apiCheck
+	describe('apiCheck', () => {
+		it('should return response of api health', () => {
+			mockHealthService.apiCheck.mockReturnValue(mockResponse);
+
+			const result = controller.apiCheck();
+
+			expect(mockHealthService.apiCheck).toHaveBeenCalled();
+			expect(result).toEqual(mockResponse);
+		});
+	});
+
+	// dbCheck
+	describe('dbCheck', () => {
+		it('should return response of database health', async () => {
+			mockHealthService.dbCheck.mockResolvedValue(mockResponse);
+
+			const result = await controller.dbCheck();
+
+			expect(mockHealthService.dbCheck).toHaveBeenCalled();
+			expect(result).toEqual(mockResponse);
+		});
+	});
 });
--- src/modules/health/health.service.spec.ts
+++ src/modules/health/health.service.spec.ts
@@ -1,18 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ServiceUnavailableException } from '@nestjs/common';
 import { HealthService } from './health.service';
+import { PrismaService } from '@/common/prisma/prisma.service';
 
 describe('HealthService', () => {
-  let service: HealthService;
+	let service: HealthService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [HealthService],
-    }).compile();
+	const mockPrisma = {
+		$queryRaw: jest.fn(),
+	};
 
-    service = module.get<HealthService>(HealthService);
-  });
+	beforeEach(async () => {
+		const module: TestingModule = await Test.createTestingModule({
+			providers: [HealthService, { provide: PrismaService, useValue: mockPrisma }],
+		}).compile();
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+		service = module.get<HealthService>(HealthService);
+		jest.clearAllMocks();
+	});
+
+	it('should be defined', () => {
+		expect(service).toBeDefined();
+	});
+
+	// apiCheck
+	describe('apiCheck', () => {
+		it('should return response of api health', () => {
+			const result = service.apiCheck();
+
+			expect(result).toMatchObject({
+				status: 'ok',
+			});
+
+			expect(result.uptime).toBeDefined();
+			expect(result.timestamp).toBeDefined();
+		});
+	});
+
+	// dbCheck
+	describe('dbCheck', () => {
+		it('should return response of database health', async () => {
+			mockPrisma.$queryRaw.mockResolvedValue([{ result: 1 }]);
+
+			const result = await service.dbCheck();
+
+			expect(result).toMatchObject({
+				status: 'ok',
+			});
+		});
+
+		it('should throw ServiceUnavaibleException when database fails', async () => {
+			mockPrisma.$queryRaw.mockRejectedValue(new Error('DB error'));
+
+			const result = service.dbCheck();
+
+			await expect(result).rejects.toThrow(ServiceUnavailableException);
+		});
+	});
 });
