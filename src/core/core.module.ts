9dcc93f667c5338837a9fb40b40b57bbad84e837
catcode <catcode0101@gmail.com>
2026-03-22 12:34:03 +0700

    feat: add meilisearch and search engine health check

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,10 +1,11 @@
 import { Module, Global } from '@nestjs/common';
 import { EnvModule } from './partials/env.module';
 import { LoggerModule } from './partials/logger.module';
+import { MeilisearchModule } from './partials/meilisearch.module';
 
 @Global()
 @Module({
-	imports: [EnvModule, LoggerModule],
-	exports: [EnvModule, LoggerModule],
+	imports: [EnvModule, LoggerModule, MeilisearchModule],
+	exports: [EnvModule, LoggerModule, MeilisearchModule],
 })
 export class CoreModule {}
--- src/modules/health/health.module.ts
+++ src/modules/health/health.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '@/common/prisma/prisma.service';
+import { MeilisearchService } from '@/common/search/meilisearch.service';
 import { HealthController } from './health.controller';
 import { HealthService } from './health.service';
 
 @Module({
 	controllers: [HealthController],
-	providers: [HealthService, PrismaService],
+	providers: [HealthService, PrismaService, MeilisearchService],
 })
 export class HealthModule {}
